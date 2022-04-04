149c16ddba783135cf64b3e2a7436b9e9bd80cc4
Kevin Grigorenko <kevin.grigorenko@us.ibm.com>
2022-04-04 12:24:12 -0500

    Change package
    Signed-off-by: Kevin Grigorenko <kevin.grigorenko@us.ibm.com>

---
+++ exercisedb-war/src/main/java/com/example/exercisedb/web/ApplicationInitializer.java
@@ -0,0 +1,26 @@
+package com.example.exercisedb.web;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@Startup
+public class ApplicationInitializer {
+
+	private static final Logger LOG = Logger.getLogger(ApplicationInitializer.class.getName());
+
+	@PostConstruct
+	private void onStartup() throws Throwable {
+		if (LOG.isLoggable(Level.INFO))
+			LOG.info(toString() + " started");
+
+		// code
+
+		if (LOG.isLoggable(Level.INFO))
+			LOG.info(toString() + " finished");
+	}
+}
--- exercisedb-war/src/main/java/com/example/exercisedb/web/ExerciseDBServlet.java
+++ exercisedb-war/src/main/java/com/example/exercisedb/web/ExerciseDBServlet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *******************************************************************************/
-package com.example.exercisedb.ejb.web;
+package com.example.exercisedb.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
